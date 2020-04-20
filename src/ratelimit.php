9513326248bed1d965ee6cf57112d20e6503e074
sailboatAnon <devnull>
2020-04-20 15:49:37 -0700

    Rate Limiting

---
+++ src/ratelimit.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+Predis\Autoloader::register();
+
+class ratelimit {
+    private $redis;
+    const RATE_LIMIT = 45; // allow 1 request every x seconds
+
+    public function __construct() {
+        $this->redis = new Predis\Client([
+            'scheme' => 'tcp',
+            'host'   => 'localhost',
+            'port'   => 6379
+        ]);
+    }
+
+    public function getSleepTime($ip) {
+        $value = $this->redis->get($ip);
+        if(empty($value)) {
+
+            $this->redis->set($ip, time()+self::RATE_LIMIT);
+            return 0;
+        } 
+        $t = self::RATE_LIMIT - (time() - intval(strval($value))) - 60;
+        if($t <= 0) {
+            $this->redis->del($ip);
+            return 0;
+        } else {
+            return $t;
+        }
+    }
+}
