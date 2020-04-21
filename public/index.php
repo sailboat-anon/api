e4a44fe6dc942538ef6c8fe068300d9f7ab46078
sailboatAnon <devnull>
2020-04-20 18:13:43 -0700

    Blacklist functionality (currently tor exit nodes)

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../src/ratelimit.php';
+if (in_array($_SERVER["REMOTE_ADDR"], file("blacklist", FILE_IGNORE_NEW_LINES))) { header("HTTP/1.1 403 Forbidden", TRUE, 403); exit; }
 include __DIR__ . '/../src/route.php';
 include __DIR__ . '/../src/whoami.php';
 include __DIR__ . '/../src/s.php';
