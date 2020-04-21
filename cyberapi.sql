9c115819ffe385ad94f34689de15cac58e387ce5
sailboatAnon <devnull>
2020-04-20 17:23:28 -0700

    Updated database (mysqldump)

--- cyberapi.sql
+++ cyberapi.sql
@@ -1,25 +1,12 @@
 -- MySQL dump 10.13  Distrib 8.0.16, for macos10.14 (x86_64)
---
--- Host: localhost    Database: cyberland
--- ------------------------------------------------------
--- Server version	8.0.16
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
  SET NAMES utf8mb4 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `cyberland`
+-- Current Database: `cyberapi`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `cyberapi` ;
+CREATE DATABASE  `cyberapi`;
 
 USE `cyberapi`;
 
@@ -32,7 +19,7 @@ DROP TABLE IF EXISTS `s`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `s` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `content` varchar(5000) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `content` varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `replyTo` int(11) unsigned DEFAULT '0',
   `bumpCount` int(11) unsigned DEFAULT '0',
   `time` datetime DEFAULT CURRENT_TIMESTAMP,
@@ -40,6 +27,26 @@ CREATE TABLE `s` (
   `modified_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tenants`
+--
+
+DROP TABLE IF EXISTS `tenants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tenants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `instance_ip` varchar(255) DEFAULT NULL,
+  `instance_name` varchar(255) DEFAULT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,12 +58,13 @@ DROP TABLE IF EXISTS `users`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sha_id` varchar(256) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `board` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `sha_id` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `board` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `modified_date` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_agent` varchar(256) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_agent` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `thread` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `SECONDARY` (`user_agent`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
